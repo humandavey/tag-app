4f29d6ed4a4dccf75d26afa89869583ee0188610
humandavey <davey.adams.three@gmail.com>
2025-05-20 21:56:39 -0500

    Initial Commit

---
+++ TAG/TAGApp.swift
@@ -0,0 +1,17 @@
+//
+//  TAGApp.swift
+//  TAG
+//
+//  Created by Davey Adams on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct TAGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TAGTests/TAGTests.swift
@@ -0,0 +1,16 @@
+//
+//  TAGTests.swift
+//  TAGTests
+//
+//  Created by Davey Adams on 5/20/25.
+//
+
+import Testing
+
+struct TAGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
