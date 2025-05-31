4b0a297cfb10e7d4a3b8fdf825683387260cee28
humandavey <davey.adams.three@gmail.com>
2025-05-31 18:14:06 -0500

    feat: onboarding screen

--- TAG/Views/ContentView.swift
+++ TAG/Views/ContentView.swift
@@ -10,44 +10,49 @@ import SwiftUI
 struct ContentView: View {
     @State private var showSelectableGamesView = false
     @State private var showProfileView = false
+    @State private var signedIn = false
     
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        ZStack {
-            NavigationStack {
-                GameView()
-                    .toolbar {
-                        ToolbarItem(placement: .topBarLeading) {
-                            Button(action: {
-                                showSelectableGamesView.toggle()
-                            }, label: {
-                                Text("The Woodlands TAG")
-                                    .font(.title2)
-                                    .tint(colorScheme == ColorScheme.dark ? .white : Color(hex: "2a2a2a"))
-                                    .bold()
-                            })
+        if signedIn {
+            ZStack {
+                NavigationStack {
+                    GameView()
+                        .toolbar {
+                            ToolbarItem(placement: .topBarLeading) {
+                                Button(action: {
+                                    showSelectableGamesView.toggle()
+                                }, label: {
+                                    Text("The Woodlands TAG")
+                                        .font(.title2)
+                                        .tint(colorScheme == ColorScheme.dark ? .white : Color(hex: "2a2a2a"))
+                                        .bold()
+                                })
+                            }
+                            ToolbarItem(placement: .topBarTrailing) {
+                                Button(action: {
+                                    showProfileView.toggle()
+                                }, label: {
+                                    Image(systemName: "person.fill")
+                                        .tint(colorScheme == ColorScheme.dark ? .white : Color(hex: "2a2a2a"))
+                                })
+                            }
                         }
-                        ToolbarItem(placement: .topBarTrailing) {
-                            Button(action: {
-                                showProfileView.toggle()
-                            }, label: {
-                                Image(systemName: "person.fill")
-                                    .tint(colorScheme == ColorScheme.dark ? .white : Color(hex: "2a2a2a"))
-                            })
-                        }
-                    }
+                }
             }
-        }
-        .sheet(isPresented: $showProfileView) {
-            ProfileView()
-                .presentationDetents([.fraction(0.9)])
-                .presentationDragIndicator(.visible)
-        }
-        .sheet(isPresented: $showSelectableGamesView) {
-            SelectableGameView()
-                .presentationDetents([.fraction(0.9)])
-                .presentationDragIndicator(.visible)
+            .sheet(isPresented: $showProfileView) {
+                ProfileView()
+                    .presentationDetents([.fraction(0.9)])
+                    .presentationDragIndicator(.visible)
+            }
+            .sheet(isPresented: $showSelectableGamesView) {
+                SelectableGameView()
+                    .presentationDetents([.fraction(0.9)])
+                    .presentationDragIndicator(.visible)
+            }
+        } else {
+            OnboardingView(signedIn: $signedIn)
         }
     }
 }
--- TAG/Views/OnboardingView.swift
+++ TAG/Views/OnboardingView.swift
@@ -1,18 +1,62 @@
-//
-//  OnboardingView.swift
-//  TAG
-//
-//  Created by Davey Adams on 5/23/25.
-//
-
 import SwiftUI
 
 struct OnboardingView: View {
+    @Binding var signedIn: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 0) {
+                Color(hex: "#B2E6EC")
+                Spacer()
+                Color(hex: "#4A4A4A")
+            }
+            .ignoresSafeArea()
+            
+            VStack {
+                Text("TAG")
+                    .font(.largeTitle)
+                    .foregroundColor(Color(hex: "#2A2A2A"))
+                    .bold()
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color(hex: "#B2E6EC"))
+
+                Image("OnboardingAsset")
+                    .resizable()
+                    .scaledToFit()
+
+                VStack {
+                    Button {
+                        signedIn = true
+                    } label: {
+                        Text("Log In")
+                            .bold()
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(32)
+                    }
+
+                    Button {
+                        
+                    } label: {
+                        Text("Sign Up")
+                            .bold()
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(32)
+                    }
+                }
+                .padding()
+                .background(Color(hex: "#4A4A4A"))
+            }
+        }
     }
 }
 
 #Preview {
-    OnboardingView()
+    OnboardingView(signedIn: .constant(false))
 }
